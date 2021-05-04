a4a86f9c3d54a9ce6ea87052336d7e0474fdd163
Mohammad Za'areer <zaa92reer@gmail.com>
2021-05-04 03:25:56 +0300

    setting up repo

---
+++ src/error-handlers/500.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = (err, req, res, next) => {
+    res.status(500).json({
+        err: err,
+        message: `somthing goes wronge ${err.message}`,
+        path: req.path
+    });
+}
---
+++ src/server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const logger = require('./middleware/logger.js');
+const validator = require('./middleware/validator.js');
+
+const errorHandler = require('./error-handlers/500.js');
+const notFoundHandler = require('./error-handlers/404.js');
+
+// Global Middleware
+app.use(logger);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to Express server!');
+});
+
+app.get('/person', validator, (req, res) => {
+    res.status(200).json({
+        name: req.query.name
+    });
+});
+
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+function start(port) {
+    app.listen(port, () => console.log(`App is running on ${port}`));
+}
+module.exports = {
+    app,
+    start
+}
